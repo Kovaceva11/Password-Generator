4735dffbc9255e5656d4387f661b9d943df59914
The Kovac <88350033+Kovaceva11@users.noreply.github.com>
2021-11-13 15:40:43 -0500

    Added all working code and updated README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Password-Generator
+
+As and employee with access to sensitive data I needed to randomly generate a password that meets the following criteria. 
+
+- must be between 8 - 128 characters in length.
+- must be able to choose including any of the following characters: Special Characters, Numbers, lower cased and upper cased letters.
+
+## How it works
+>This application works using Javascript to define a set of arrays (i.e. special characters, numbers, lowercase, uppercase.). These arrays are then called upon in different functions and conditional statements that prompt the user to select the option of including certain type of characters in the password. 
+
+>The application sets the selected password Options as an object. Then the GeneratePassword function calls upon that object's properties to get random indices from the initial arrays.
+
+If Cancel is selected in the window alert prompt; then the character type will NOT be included in the password result. 
+
+## Credits
+MDN Web Docs [https://developer.mozilla.org/en-US/](Developer.Mozilla.Org)
+
+Georgia Tech Full-Stack Bootcamp - The starter code of the Index.Html, CSS and JS file.
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,100 @@
 // Create Arrays for Special Characters, Numbers, lowerCase & upperCase
+var specialCharacters = [ 
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
+];
+
+var numbers = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+];
+
+var lowerCase = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+];
+
+var upperCase =[
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+];
 
 // Create Function to prompt the user for the password options
     // Check if password length is a number. Return if false.
@@ -7,8 +103,60 @@
     // Store boolean to confirm if user wants Special Characters, numbers, lowercase, uppercase. 
     //Check if user does not include any types, the generator will return only if all variables are false.
     // Create an object for the user's input
+function Options() {
+    var length = parseInt(
+        prompt('how many characters would you like your password to contain? Must be between 8-128 characters.')
+    );
+
+    if (isNaN(length === true)) {
+        alert('Password length must be provided as a number');
+        return;
+    }
+
+    if (length < 8) {
+        alert('Password length must be at least 8 characters long')
+        return;
+    }
+
+    if (length > 128) {
+        alert('Password length must be less than 129 chracters')
+        return;
+    }
+
+    var includesSpecialCharacters = confirm('Click OK to add Special Characters');
+    var includesNumbers = confirm('Click OK to add Numbers');
+    var includeslowerCase = confirm('Click OK to add lower-cased Characters');
+    var includesupperCase = confirm('Click OK to add upper-cased Characters');
+
+    if (
+        includesSpecialCharacters === false &&
+        includesNumbers === false &&
+        includeslowerCase === false &&
+        includesupperCase === false
+    ) {
+        alert('Must choose at least one character option')
+        return;
+    }
+
+    var selectedOptions = {
+        length: length,
+        includesSpecialCharacters: includesSpecialCharacters,
+        includesNumbers: includesNumbers,
+        includeslowerCase: includeslowerCase,
+        includesupperCase: includesupperCase,
+    };
+
+    return selectedOptions;
+}
+
 
 // Gets random element from an array.
+function getRandom(array) {
+    var randIndex = Math.floor(Math.random()* array.length);
+    var randElement = array[randIndex];
+
+    return randElement;
+}
 
 // Generate the password with user's inputs
     // array password result
@@ -18,8 +166,46 @@
     // for loop to use password length from the options object. Selects random from the possible characters. Pushing to the result.
     // needs to use at least 1 of each selected character.
     // change to string and write to password. 
+function generatePassword() {
+    var selected = Options();
+    var result = [];
+    var allCharacters = [];
+    var selectedCharacters = [];
+
+    if (selected.includesSpecialCharacters) {
+        allCharacters = allCharacters.concat(specialCharacters);
+        selectedCharacters.push(getRandom(specialCharacters));
+    }
+
+    if (selected.includesNumbers) {
+        allCharacters = allCharacters.concat(numbers);
+        selectedCharacters.push(getRandom(numbers));
+    }
+
+    if (selected.includeslowerCase) {
+        allCharacters = allCharacters.concat(lowerCase);
+        selectedCharacters.push(getRandom(lowerCase));
+    }
+
+    if (selected.includesupperCase) {
+        allCharacters = allCharacters.concat(upperCase);
+        selectedCharacters.push(getRandom(upperCase));
+    }
+
+    for (var i = 0; i <selected.length; i++) {
+        var allCharacters = getRandom(allCharacters);
+        result.push(allCharacters);
+    }
     
-// Assignment Code
+    for (var i=0; i < selectedCharacters.length; i++) {
+        result[i] = selectedCharacters[i];
+    }
+
+    return result.join('');
+}
+
+    
+// Assignment Starter Code Below
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
