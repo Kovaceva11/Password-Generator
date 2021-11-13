7221a92772b174ce9b8edaa08699f636c9bbe1f3
The Kovac <88350033+Kovaceva11@users.noreply.github.com>
2021-11-13 16:47:13 -0500

    Rewrote Script File using ASCII CharCodes for cleaner code.

--- assets/script.js
+++ assets/script.js
@@ -1,208 +1,60 @@
-// Create Arrays for Special Characters, Numbers, lowerCase & upperCase
-var specialCharacters = [ 
-    '@',
-    '%',
-    '+',
-    '\\',
-    '/',
-    "'",
-    '!',
-    '#',
-    '$',
-    '^',
-    '?',
-    ':',
-    ',',
-    ')',
-    '(',
-    '}',
-    '{',
-    ']',
-    '[',
-    '~',
-    '-',
-    '_',
-    '.'
-];
 
-var numbers = [
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-];
-
-var lowerCase = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
-];
-
-var upperCase =[
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
-];
-
-// Create Function to prompt the user for the password options
-    // Check if password length is a number. Return if false.
-    // Check if password length is at least 8 characters long. Prompts return if false.
-    // Check if password length is less than 128 characters long. Prompts return if false.
-    // Store boolean to confirm if user wants Special Characters, numbers, lowercase, uppercase. 
-    //Check if user does not include any types, the generator will return only if all variables are false.
-    // Create an object for the user's input
-function Options() {
-    var length = parseInt(
-        prompt('how many characters would you like your password to contain? Must be between 8-128 characters.')
-    );
-
-    if (isNaN(length === true)) {
-        alert('Password length must be provided as a number');
-        return;
+// generates array from the low number to the high number of the ASCII character codes.
+const arrayFromLowToHighNum = (low, high) => {
+    const array = [];
+    for (let i = low; i <= high; i++) {
+      array.push(i);
     }
-
-    if (length < 8) {
-        alert('Password length must be at least 8 characters long')
-        return;
+    return array;
+  };
+//   Creating Arrays for Character Codes from ASCII Codes.
+  const UPPERCASE_CODES = arrayFromLowToHighNum(65, 90);
+  const LOWERCASE_CODES = arrayFromLowToHighNum(97, 122);
+  const NUMBER_CODES = arrayFromLowToHighNum(48, 57);
+  const SYMBOLS_CODES = arrayFromLowToHighNum(33, 47)
+    .concat(arrayFromLowToHighNum(58, 64))
+    .concat(arrayFromLowToHighNum(91, 96))
+    .concat(arrayFromLowToHighNum(123, 126));
+  
+  // Function to determine password Length
+  function generatePassword() {
+    var passwordLength = +window.prompt("Choose a password length between 8 & 128")
+    if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("You must enter a password length between 8 and 128 characters.");
+      return "";
     }
-
-    if (length > 128) {
-        alert('Password length must be less than 129 characters')
-        return;
+    // selected variables for the password.
+    var lowerCase = window.confirm("Would you like to use lower case characters?");
+    var upperCase = window.confirm("Would you like to use upper case characters?");
+    var numbers = window.confirm("Would you like to use numbers?");
+    var specialCharacters = window.confirm("Would you like special characters?");
+  
+    // creates character codes array for password to generate using the selected variables from above.
+    let charCodes = [];
+    if (lowerCase) {
+      charCodes = charCodes.concat(LOWERCASE_CODES);
     }
-
-    var includesSpecialCharacters = confirm('Click OK to add Special Characters');
-    var includesNumbers = confirm('Click OK to add Numbers');
-    var includeslowerCase = confirm('Click OK to add lower-cased Characters');
-    var includesupperCase = confirm('Click OK to add upper-cased Characters');
-
-    if (
-        includesSpecialCharacters === false &&
-        includesNumbers === false &&
-        includeslowerCase === false &&
-        includesupperCase === false
-    ) {
-        alert('Must choose at least one character option')
-        return;
+    if (upperCase) {
+      charCodes = charCodes.concat(UPPERCASE_CODES);
     }
-
-    var selectedOptions = {
-        length: length,
-        includesSpecialCharacters: includesSpecialCharacters,
-        includesNumbers: includesNumbers,
-        includeslowerCase: includeslowerCase,
-        includesupperCase: includesupperCase,
-    };
-
-    return selectedOptions;
-}
-
-
-// Gets random element from an array.
-function getRandom(array) {
-    var randIndex = Math.floor(Math.random( ) * array.length);
-    var randElement = array[randIndex];
-
-    return randElement;
-}
-
-// Generate the password with user's inputs
-    // array password result
-    // array types of characters in the result
-    // array for selected characters
-    // Add array of Special/number/lower/upper Characters into total characters. Push new random character to selectedCharacters
-    // for loop to use password length from the options object. Selects random from the possible characters. Pushing to the result.
-    // needs to use at least 1 of each selected character.
-    // change to string and write to password. 
-function generatePassword() {
-    var selected = Options();
-    var result = [];
-    var allCharacters = [];
-    var selectedCharacters = [];
-
-    if (selected.includesSpecialCharacters) {
-        allCharacters = allCharacters.concat(specialCharacters);
-        selectedCharacters.push(getRandom(specialCharacters));
+    if (specialCharacters) {
+      charCodes = charCodes.concat(SYMBOLS_CODES);
     }
-
-    if (selected.includesNumbers) {
-        allCharacters = allCharacters.concat(numbers);
-        selectedCharacters.push(getRandom(numbers));
+    if (numbers) {
+      charCodes = charCodes.concat(NUMBER_CODES);
     }
-
-    if (selected.includeslowerCase) {
-        allCharacters = allCharacters.concat(lowerCase);
-        selectedCharacters.push(getRandom(lowerCase));
+    if (charCodes.length === 0) {
+      alert("Please choose at least one set of characters");
+      return ""
     }
+    const passwordCharacters = Array.from({length: passwordLength}, () => {
+      const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)];
+      return String.fromCharCode(characterCode);
+    })
+    return passwordCharacters.join('');
+  }
+  
 
-    if (selected.includesupperCase) {
-        allCharacters = allCharacters.concat(upperCase);
-        selectedCharacters.push(getRandom(upperCase));
-    }
-
-    for (var i = 0; i <selected.length; i++) {
-        var allCharacters = getRandom(allCharacters);
-        result.push(allCharacters);
-    }
-    
-    for (var i=0; i < selectedCharacters.length; i++) {
-        result[i] = selectedCharacters[i];
-    }
-
-    return result.join('');
-}
 
     
 // Assignment Starter Code Below
